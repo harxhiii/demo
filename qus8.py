cdfa878c4771edb649177165b5488db2b67d3ad3
harshita <144467188+harxhiii@users.noreply.github.com>
2024-06-19 19:43:47 +0530

    Add files via upload

---
+++ qus8.py
@@ -0,0 +1,10 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a= input(" enter string1 : ")
+ enter string1 : harshita
+>>> b= input(" enter string2: ")
+ enter string2: panwar
+>>> concatenated_string= a+b
+>>> print(f" the concatenated string is {concatenated_string}")
+ the concatenated string is harshitapanwar
+>>> 
