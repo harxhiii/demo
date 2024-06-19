8eb13404657b9ab84c8bdf9b329285bef4a32ae5
harshita <144467188+harxhiii@users.noreply.github.com>
2024-06-19 22:30:46 +0530

    Add files via upload

---
+++ qus14.py
@@ -0,0 +1,12 @@
+lines = []
+print("Enter multiple lines of input and then Enter an empty line to finish.")
+
+while True:
+    line = input()
+    if line == "":
+        break
+    lines.append(line)
+
+print("\nYou entered:")
+for line in lines:
+    print(line)
