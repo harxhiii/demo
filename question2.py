01f27e6747e9a56934e5d1f5e00f38bd2de97abf
harshita <144467188+harxhiii@users.noreply.github.com>
2024-06-19 18:41:15 +0530

    Add files via upload

---
+++ question2.py
@@ -0,0 +1,12 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("enter the number: "))
+enter the number: 10
+>>> if(a%2==0):
+	print("a is an even number")
+else:
+	print("a is an odd number")
+
+	
+a is an even number
+>>> 
