88d8e273d369e84f078f4dbf97f0e6d5bba2be54
harshita <144467188+harxhiii@users.noreply.github.com>
2024-06-19 18:53:46 +0530

    Add files via upload

---
+++ question3.py
@@ -0,0 +1,16 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a= int(input(" enter the given number: "))
+ enter the given number: 5
+>>> prod=1
+>>> for i in range(1,a+1):
+	prod*=i
+	print("factorial of the given number is: ",prod)
+
+	
+factorial of the given number is:  1
+factorial of the given number is:  2
+factorial of the given number is:  6
+factorial of the given number is:  24
+factorial of the given number is:  120
+>>> 
