f4c89fd7dd93bd2c5e3a09a420342ec4ee5c53c1
harshita <144467188+harxhiii@users.noreply.github.com>
2024-06-19 21:53:23 +0530

    Add files via upload

---
+++ qus11.py
@@ -0,0 +1,21 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=0
+>>> b=1
+>>> n= int(input(" enter the number: "))
+ enter the number: 10
+>>> for i in range(n-1):
+	print(b)
+	a,b=b,a+b
+
+	
+1
+1
+2
+3
+5
+8
+13
+21
+34
+>>> 
