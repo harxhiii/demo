f135249a33774441d93a532a381193d45d0cdac8
harshita <144467188+harxhiii@users.noreply.github.com>
2024-06-19 21:26:53 +0530

    Add files via upload

---
+++ qus9.py
@@ -0,0 +1,26 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> mainstring= input("enter the main string")
+enter the main string football
+>>> substring=input(" enter the substring that you wants to check")
+ enter the substring that you wants to check ball
+>>> if substring in mainstring:
+	print(f" mainstring includes '{substring}' in it.")
+else :
+	print(f"mainstring do not includes '{substring}' in it.")
+
+	
+mainstring do not includes ' ball' in it.
+>>> mainstring= input("enter the main string")
+enter the main string foot ball
+>>> substring=input(" enter the substring that you wants to check")
+ enter the substring that you wants to check ball
+>>> if substring in mainstring:
+	print(f" mainstring includes '{substring}' in it.")
+else :
+	print(f"mainstring do not includes '{substring}' in it.")
+	
+enter the main string
+>>> 
+>>> 
+>>> 
