9e47be438a1619f728c850f62fdd0399a3e5c549
harshita <144467188+harxhiii@users.noreply.github.com>
2024-06-19 17:44:56 +0530

    Add files via upload

---
+++ question1.py
@@ -0,0 +1,9 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a= int(input("first number is: "))
+first number is: 20
+>>> b= int(input("second number is: "))
+second number is: 50
+>>> print("the sum of a and b is: ",a,b,a+b)
+the sum of a and b is:  20 50 70
+>>> 
