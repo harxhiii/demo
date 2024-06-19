f67985c8d5b7948fb94d54127cbb124765a3477c
harshita <144467188+harxhiii@users.noreply.github.com>
2024-06-19 19:29:14 +0530

    Add files via upload

---
+++ question7.py
@@ -0,0 +1,8 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a= input("enter a string: ")
+enter a string: harshita
+>>> length_of_string=len(a)
+>>> print(f"the length of the string '{a}' is {length_of_string}")
+the length of the string 'harshita' is 8
+>>> 
