f90e61248227d08dd8ada2d61954ff0c0a9efdc6
harshita <144467188+harxhiii@users.noreply.github.com>
2024-06-19 19:00:09 +0530

    Add files via upload

---
+++ question4.py
@@ -0,0 +1,7 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x= input(" enter your name: ")
+ enter your name: harshita
+>>> print("hey!!",x,"i'm so glad that i met you...hope we meet again")
+hey!! harshita i'm so glad that i met you...hope we meet again
+>>> 
