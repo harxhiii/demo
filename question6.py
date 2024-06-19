e6a5875b8475566a5b4090ea5017da132014d79e
harshita <144467188+harxhiii@users.noreply.github.com>
2024-06-19 19:17:20 +0530

    Add files via upload

---
+++ question6.py
@@ -0,0 +1,8 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=open(r"C:\Users\hp\AppData\Local\Programs\Python\Python39\text.py","r")
+>>> y=x.read()
+>>> print(y)
+
+>>> 
+>>> 
